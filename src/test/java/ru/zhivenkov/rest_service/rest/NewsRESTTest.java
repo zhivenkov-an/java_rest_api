bce35e0d1aa6141df16c2c7d5575d43fbfbbf554
zhivenkov <zhivenkov@mail.ru>
2019-07-07 23:58:24 +0300

    4 commit. Add Junit test for core services

--- src/test/java/ru/zhivenkov/rest_service/rest/NewsRESTTest.java
+++ src/test/java/ru/zhivenkov/rest_service/rest/NewsRESTTest.java
@@ -1,24 +1,69 @@
 package ru.zhivenkov.rest_service.rest;
 
-import org.junit.Test;
-import org.mockito.Mock;
-import ru.zhivenkov.rest_service.repository.NewsRepository;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.HttpClientBuilder;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class NewsRESTTest {
 
-    private NewsREST subject;
-
-    @Mock
-    private NewsRepository newsRepository;
 
     @Test
-    public void getAll() {
+    public void restApiForbided()
+            throws ClientProtocolException, IOException {
+
+        // Given
+
+        HttpUriRequest request = new HttpGet( "http://127.0.0.1:8080/newsRest/api/news");
+
+        // When
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+
+        // Then
+        assertThat(
+                httpResponse.getStatusLine().getStatusCode(),
+                equalTo(HttpStatus.SC_OK));
     }
 
     @Test
-    public void getById() {
+    public void getAll() throws IOException {
+
+        // Given
+        String jsonMimeType = "application/json";
+        HttpUriRequest request = new HttpGet( "http://127.0.0.1:8080/newsRest/api/news" );
+
+        // When
+        HttpResponse response = HttpClientBuilder.create().build().execute( request );
+
+        // Then
+        String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+        assertEquals( jsonMimeType, mimeType );
+
+    }
+
+    @Test
+    public void getById() throws IOException {
+        // Given
+        String jsonMimeType = "application/json";
+        HttpUriRequest request = new HttpGet( "http://127.0.0.1:8080/newsRest/api/news/1" );
+
+        // When
+        HttpResponse response = HttpClientBuilder.create().build().execute( request );
+
+        // Then
+        String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+        assertEquals( jsonMimeType, mimeType );
+
     }
 
     @Test
