dcba124e156066fdbcd89b038aea404865e8564c
zhivenkov <zhivenkov@mail.ru>
2019-07-07 21:11:17 +0300

    third commit. Add simple unit test

---
+++ src/test/java/ru/zhivenkov/rest_service/rest/NewsRESTTest.java
@@ -0,0 +1,31 @@
+package ru.zhivenkov.rest_service.rest;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import ru.zhivenkov.rest_service.repository.NewsRepository;
+
+import static org.junit.Assert.*;
+
+public class NewsRESTTest {
+
+    private NewsREST subject;
+
+    @Mock
+    private NewsRepository newsRepository;
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void addNews() {
+    }
+}
